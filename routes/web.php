636bdf5cbf3c8532c8216edf4fdde687dd8e11cc
Isac Miael <Iscgoncalvesbrito@gmail.com>
2026-08-26 16:12:21 -0300

    oficina e web alterados

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LivroControler;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
 
@@ -8,6 +9,12 @@ Route::get('/', function () {
 });
 
 Route::view('/landing', 'landing');
+
+Route::get('/oficinas', [OficinaController::class, 'index']);
+Route::post('/oficinas', [OficinaController::class, 'store']);
+
+Route::get('/livros', [LivroControler::class, 'index']);
+Route::post('/livros', [LivroControler::class, 'store']);
 
 Route::get('/produtos', [ProdutoController::class, 'index']);
 Route::post('/produtos', [ProdutoController::class, 'store']);
